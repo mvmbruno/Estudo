00ec12bca8e0159472e1e2cd7fca98308ca03740
Bruno Marques <brunomvm1@gmail.com>
2018-10-25 20:07:13 -0300

    Atualização da Calculadora

--- Udemy/curso-javascript-projeto-calculadora-clone/Scripts/Controller/calcController.js
+++ Udemy/curso-javascript-projeto-calculadora-clone/Scripts/Controller/calcController.js
@@ -1,17 +1,60 @@
 class CalcController {
 
     constructor(){
-        this._displayCalc = '0';
-        this._currentDate;
+			this._locale = 'pt-BR';
+			this._displayCalcEl = document.querySelector("#display");
+			this._dateEl = document.querySelector("#data");
+			this._timeEl = document.querySelector("#hora");
+			this._currentDate;
+			this.initalize();
+			this.initButtonEvents();
     }    
+
+		initalize(){
+			setInterval(()=>{
+				this.setDisplayDateTime();
+			}, 1000)
+		}
+
+		initButtonEvents(){
+			let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+			buttons.forEach((btn, index)=>{
+				btn.addEventListener('click',e=>{
+					console.log(btn.className.baseVal.replace("btn-",""));
+				})
+			})
+		}
+
+		setDisplayDateTime(){
+			this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+			this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+		}
+
+		get displayTime(){
+			return this._timeEl.innerHTML;
+		}
+
+		set displayTime(value){
+			return this._timeEl.innerHTML = value;
+		}
+
+		get displayDate(){
+			return this._dateEl.innerHTML;
+		}
+
+		set displayDate(value){
+			return this._dateEl.innerHTML = value;
+		}
+
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
-    set displayCalc(valor){
-        this._displayCalc = valor;
+    set displayCalc(value){
+			this._displayCalcEl.innerHTML = value;
     }
     get currentDate(){
-        return this._dataAtual;
+        return new Date();
     }
     set currentDate(valor){
         this._currentDate = valor;
